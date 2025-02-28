dbe1ce278ed9965cf0b2435a0b0e343b2f671faf
rajvindergithub <rajvindergithub@gmail.com>
2025-02-28 17:08:51 +0530

    add plugin page for option page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 
 /*
- * Plugin Name:       My Basics Plugin
+ * Plugin Name:       Add Option Page
  * Description:       Handle the basics with this plugin.
  * Version:           1.10.3
  * Requires at least: 5.2
@@ -12,9 +12,24 @@
  * Domain Path:       /languages
  */
 
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
 
+add_action('admin_menu', 'cp_add_menu_page');
+
+function cp_add_menu_page() {
+ 
+    add_menu_page('Option Page Plugin', 'Theme Option', 'manage_options', 'theme-option-page','option_page_handle_function','dashicons-admin-home', 20);
+}
+ 
+function option_page_handle_function(){
+?>
+    <section id="option_page_wordpress_plug">
+        <div><h1>Theme Option Page</h1></div>
+    </section>
     
-
+<?php }
 
 
 
