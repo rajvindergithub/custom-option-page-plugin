4bd3eb92a4d123b226ff6a7047b4890eb41c366f
rajvindergithub <rajvindergithub@gmail.com>
2025-02-28 15:30:52 +0530

    update plugin

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+/*
+ * Plugin Name:       My Basics Plugin
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Rajvinder singh
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
+
+
+    
+
+
+
+
+?>
